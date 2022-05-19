8ea51e2cf45b018dacaac8a867225484c8bde66e
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-05-20 00:30:21 +0100

    initial cleanup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Head from 'next/head'
+import { motion } from 'framer-motion'
+
+
+const Home = () => {
+
+
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
